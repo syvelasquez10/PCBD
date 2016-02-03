14675cbbeb32600d2dc6dc3b0a361152e7f201ab
Sergio Velásquez <sy.velasquez10@uniandes.edu.co>
2016-02-02 19:00:36 -0500

    nuevo

--- PCBD.web/src/main/java/co/edu/uniandes/andar/PCBD/services/BookService.java
+++ PCBD.web/src/main/java/co/edu/uniandes/andar/PCBD/services/BookService.java
@@ -8,12 +8,15 @@ import javax.inject.Inject;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.OPTIONS;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 @Path("/books")
 @Consumes(MediaType.APPLICATION_JSON)
@@ -51,5 +54,10 @@ public class BookService {
     @Path("{id: \\d+}")
     public void deleteBook(@PathParam("id") Long id) {
         bookLogic.deleteBook(id);
+    }
+    
+    @OPTIONS
+    public Response cors(@javax.ws.rs.core.Context HttpHeaders requestHeaders) {
+        return Response.status(200).header("Access-Control-Allow-Origin", "*").header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS").header("Access-Control-Allow-Headers", "AUTHORIZATION, content-type, accept").build();
     }
 }
